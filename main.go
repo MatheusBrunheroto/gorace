c44958c5d2953ef908d7a18d49b738b8ef18e1d3
Matheus <matheus.brunheroto@gmail.com>
2026-05-20 10:29:35 -0300

    Main Documentation
    Simplified, I'll maybe add more fluxograms inside the files mentioned in main

--- main.go
+++ main.go
@@ -11,37 +11,33 @@ import (
 	"os"
 )
 
-// usage example: gorace -u 'https://website.com' -h '{header_name:header_value, h2_name:WORDLIST1}' -c '{WORDLIST2:WORDLIST3}' -t 50 --no-filter
+// BASIC TEST: go run main.go -u '1.com' --threads 10 -u '2.com' --threads 20
+
+// 1. Start Cache, the cache channel will be used to avoid reprocessing websites to workers in worker.go.
+// 2. Start Display, the display channel will be used to display any kind of thing in worker.go.
+/* (This means a worker will be called with both channels as parameters, cache and display). */
+// 3. Read the CLI input, will treat the wordlists, websites, etc. So it can be sent to worker.go later on.
+// 4. Start Workers, it reads and writes on Cache, and writes on Display (Progress)
 /*
-	MODES:
-		-c --cli, enables **command line mode**
-			The CLI mode will take only one website a time, unless a wordlist is provided,
-			in this case, will use two different modes, pitchfork (default) or clusterbomb
-
-		-i --interative, enables **interative mode**
-			The interative mode is not very pratical, but can be used to generate your own requests and save them in a
-			custom wordlist
-
-		-w --wordlist, enables **wordlist mode**
-			It's different from the --cli wordlists, it takes pre-made requests and make them, it's recommended to create the
-			wordlists using the interative mode
-
-	OPTIONS:
-		--no-filter, skips the data filtering, "wrong" parameters will be able to pass, for example:
-		using a header with ":HEADER_NAME:::::Content%!%!%@123123" will be passed in it's raw form to the request"
-
-		-t --threads, number of workers or agents to be used in the test, default=50
-
-go main.go run -u '1.com' --threads 10 -u '2.com' --threads 20
-
-
-*/
-// Fazer SINGLEPACKET, apenas pra modos FLOOD
-// Starts output
+ *                      THREADS
+ *                   1. ┌─────┐   CHANNELS
+ *                 ┌────┤Cache│◄──────────┐
+ *                 │    └─────┘           │
+ * ┌────────┐      │ 2. ┌───────┐         │
+ * │ main.go├──────┴────┤Display│◄─────┐  │
+ * └──┬─▲──┬┘           └───────┘      │  │
+ * 3. │ │  │                         ┌─┴──▼─┐
+ *    │ │  └─────────────────────────┤Worker│
+ *   ┌┴─┴┐    4.  init(websites)     └──────┘
+ *   │CLI│
+ *   └───┘
+ *   return
+ *  websites
+ */
 
 func main() {
 
-	// CACHE
+	// Cache (Will later on avoid buildRequest generating the same request multiple times)
 	cacheChan := make(chan cache.Operation)
 	go cache.Run(cacheChan) // OwO
 
@@ -52,32 +48,34 @@ func main() {
 		Failed:    make(chan int),
 	}
 
-	// DISPLAY ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+	// Display
 	session := display.NewSession(progress.Reader())
 	go display.Run(session)
-	<-session.Ready
+	<-session.Ready // Waits for ascii_art.go
 
-	// Read and filter the CLI inputs
-	// session.Draw <- "⸺" // ⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺
+	// CLI (Read and filter the inputs)
 	websites, mode, err := input.RunCLI(os.Args[1:])
 	if err != nil {
-		fmt.Println(err, "\nExiting...\n")
+		fmt.Println(err, "\nExiting...")
+		fmt.Println("")
 		return
 	}
 	// session.Draw <- "⸺" // ⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺⸺
 
-	// This is a memory that runs with the code, avoiding buildRequest to generate the same request multiple times
-
+	// Worker
 	workerChans := request.WorkerChans{
 		Progress:  progress.Writer(),
 		CacheChan: cacheChan,
 	}
 	request.InitWorkers(websites, mode, workerChans)
-	// ADD val padrao de threads pra 0
+
 	fmt.Printf("\n\n")
-	<-session.Finished // Waits for display output to finish
+	<-session.Finished // Waits for display output of the current session to finish
 
 }
 
+// ADD val padrao de threads pra 0
 // TODO, MODO de input direto de wordlist, MODOS DE rodar,
 // AO inves de retornar os erros e tentar fazer funcionar denovo basta colocar o panic ao inves do erros.New
+// Fazer SINGLEPACKET, apenas pra modos FLOOD
+// Starts output
